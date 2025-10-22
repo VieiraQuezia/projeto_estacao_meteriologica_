a6899b9c3ee08b0a31b51961d8c99ef9ad171a1e
Quezia Vieira <vieiraquezia24@gmail.com>
2025-10-22 14:10:32 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import Header from './components/header';
 import Footer from './components/footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
 
 import './App.css';
+import { Router } from 'lucide-react';
 
 export function AppRouter() {
   return (
+    <BrowserRouter>
       <div className="app">
         <Header />
-        <div className="app-main">
-          ro
-        </div>
+        <main className="app-main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/projeto" element={<Projeto />} />
+            <Route path="/simulacao" element={<Simulacao />} />
+            <Route path="/hardware" element={<Hardware />} />
+            <Route path="/software" element={<Software />} />
+            <Route path="/perguntas" element={<Perguntas />} />
+            <Route path="/resultados" element={<Resultados />} />
+            <Route path="/equipe" element={<Equipe />} /> */}
+          </Routes>
+        </main>
         <Footer />
       </div>
+      </BrowserRouter>
   );
 }
 export default AppRouter;
